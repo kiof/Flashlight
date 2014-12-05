18e6ca11312e25d2241da2ab7dbb98a903aeaff9
kiof <kiof@free.fr>
2014-12-05 20:23:55 +0100

    New Admob SDK

--- flashlight/build/generated/source/buildConfig/debug/com/kiof/flashlight/BuildConfig.java
+++ flashlight/build/generated/source/buildConfig/debug/com/kiof/flashlight/BuildConfig.java
@@ -5,9 +5,9 @@ package com.kiof.flashlight;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.kiof.flashlight";
+  public static final String APPLICATION_ID = "com.kiof.flashlight";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 6;
-  public static final String VERSION_NAME = "1.5";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.6";
 }
